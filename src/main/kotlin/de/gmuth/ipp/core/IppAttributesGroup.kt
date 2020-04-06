794a7b142d93124b7ccebaa7f1283706c3ca1848
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-06 23:02:31 +0200

    reject empty values

--- src/main/kotlin/de/gmuth/ipp/core/IppAttributesGroup.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppAttributesGroup.kt
@@ -11,10 +11,14 @@ class IppAttributesGroup(val tag: IppTag) : LinkedHashMap<String, IppAttribute<*
     }
 
     fun put(attribute: IppAttribute<*>): IppAttribute<*>? {
+        if (attribute.tag != IppTag.NoValue && attribute.values.isEmpty())
+            throw IppException("missing attribute values to put")
         val replaced = put(attribute.name, attribute)
         if (replaced != null) println("replaced '$replaced' with '$attribute'")
         return replaced
     }
+
+    fun attribute(name: String, tag: IppTag, values: List<*>) = put(IppAttribute(name, tag, values.toMutableList()))
 
     fun attribute(name: String, tag: IppTag, vararg value: Any?) = put(IppAttribute(name, tag, value.toMutableList()))
 
