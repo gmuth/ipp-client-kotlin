e206367e043f27d60daece2abc1d4607642a09ad
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-10 11:38:52 +0200

    structure for resolution

---
+++ src/main/kotlin/de/gmuth/ipp/core/IppResolution.kt
@@ -0,0 +1,19 @@
+package de.gmuth.ipp.core
+
+/**
+ * Copyright (c) 2020 Gerhard Muth
+ */
+
+class IppResolution(val x: Int, val y: Int, val unit: Int) {
+
+    override fun toString() = "${x}${if (x == y) "" else "x${y}"}${unitValue()}"
+
+    // where is the resolution unit specified?
+    // https://github.com/apple/cups/blob/master/cups/ipp.h
+    private fun unitValue() = when (unit) {
+        3 -> "dpi"
+        4 -> "dpcm"
+        else -> "unit-$unit"
+    }
+
+}
