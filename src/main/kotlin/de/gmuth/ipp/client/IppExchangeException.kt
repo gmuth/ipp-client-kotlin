0e90d7402bce8ac709874b379591525be2473fd7
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-11 13:18:55 +0200

    add cause parameter for constructor

--- src/main/kotlin/de/gmuth/ipp/client/IppExchangeException.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppExchangeException.kt
@@ -1,11 +1,17 @@
 package de.gmuth.ipp.client
 
-import de.gmuth.ipp.core.IppException
-import de.gmuth.ipp.core.IppRequest
-import de.gmuth.ipp.core.IppResponse
-
 /**
  * Copyright (c) 2020 Gerhard Muth
  */
 
-class IppExchangeException(val request: IppRequest, val response: IppResponse, message: String) : IppException(message)
+import de.gmuth.ipp.core.IppException
+import de.gmuth.ipp.core.IppRequest
+import de.gmuth.ipp.core.IppResponse
+
+class IppExchangeException(
+        val request: IppRequest,
+        val response: IppResponse? = null,
+        message: String,
+        cause: Exception? = null
+
+) : IppException(message, cause)
