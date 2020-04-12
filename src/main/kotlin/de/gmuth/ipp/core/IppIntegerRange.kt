cf539c83c4633d0ca6abbbc73288f9491fe84238
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-13 01:27:45 +0200

    reformatting

--- src/main/kotlin/de/gmuth/ipp/core/IppIntegerRange.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppIntegerRange.kt
@@ -4,8 +4,9 @@ package de.gmuth.ipp.core
  * Copyright (c) 2020 Gerhard Muth
  */
 
-data class IppIntegerRange(val start: Int, val end: Int) {
-
+data class IppIntegerRange(
+        val start: Int,
+        val end: Int
+) {
     override fun toString() = "$start-$end"
-
 }
