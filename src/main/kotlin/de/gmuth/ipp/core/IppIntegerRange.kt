d4a4c6ab12203fc02f11a3dc47f9799850d42aa0
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-12 22:58:51 +0200

    removed comment

--- src/main/kotlin/de/gmuth/ipp/core/IppIntegerRange.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppIntegerRange.kt
@@ -4,8 +4,6 @@ package de.gmuth.ipp.core
  * Copyright (c) 2020 Gerhard Muth
  */
 
-// Kotlin IntRange not used in favour of portability and overriding toString()
-
 class IppIntegerRange(val start: Int, val end: Int) {
 
     override fun toString() = "$start-$end"
