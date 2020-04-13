cedff3aa9c0fe5833157722c14ea8798826d4e36
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-14 01:32:11 +0200

    resolve enum integer values to  name - ipp registrations section 6

--- src/main/kotlin/de/gmuth/ipp/core/IppAttribute.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppAttribute.kt
@@ -1,19 +1,42 @@
 package de.gmuth.ipp.core
 
+import de.gmuth.ipp.iana.IppRegistrations
+import de.gmuth.ipp.iana.IppRegistrationsSection6
+
 /**
  * Copyright (c) 2020 Gerhard Muth
  */
 
-class IppAttribute<T> constructor(val name: String, val tag: IppTag, val values: MutableList<T>) {
+class IppAttribute<T> constructor(val name: String, val tag: IppTag) {
+
+    val values = mutableListOf<T>()
 
     init {
-        if (tag.isGroupTag()) throw IppException("group tag '$tag' must not be used for attributes")
+        if (tag.isGroupTag()) {
+            throw IppException("group tag '$tag' must not be used for attributes")
+        }
     }
 
-    constructor(name: String, tag: IppTag, vararg values: T) : this(name, tag, values.toMutableList())
+    companion object {
+        var allowAutomaticTag: Boolean = true
+    }
 
-    constructor(name: String, vararg values: T) : this(name, IppRegistrations.tagForAttribute(name), values.toMutableList()) {
-        if (!supportTagForAttribute) throw IppException("for '$name' use IppTag.${tag.name}")
+    constructor(name: String, tag: IppTag, vararg values: T) : this(name, tag, values.toList())
+
+    constructor(name: String, tag: IppTag, values: Collection<T>) : this(name, tag) {
+        if (values.size > 1 || values.first() != null) {
+            this.values.addAll(values)
+        }
+    }
+
+    // ------ use automatic tag
+
+    constructor(name: String, vararg values: T) : this(name, values.toList())
+
+    constructor(name: String, values: Collection<T>) : this(name, IppRegistrations.tagForAttribute(name), values) {
+        if (!allowAutomaticTag) {
+            throw IppException("for '$name' use IppTag.${tag.name}")
+        }
     }
 
     @Suppress("UNCHECKED_CAST")
@@ -33,13 +56,22 @@ class IppAttribute<T> constructor(val name: String, val tag: IppTag, val values:
             if (values.size == 1) values.first()
             else throw IppException("found ${values.size.toPluralString("value")} but expected only 1 for '$name'")
 
+    private fun valueOrEnumValueName(value: Any?): Any? =
+            if (tag == IppTag.Enum) {
+                IppRegistrationsSection6.getEnumValueName(name, value!!)
+            } else {
+                value
+            }
+
     override fun toString(): String {
-        val valuesString = if (values == null || values.isEmpty()) "" else "= " + try {
-            values.joinToString(",")
+        val valuesString = if (values.isEmpty()) "no-value" else try {
+            values.joinToString(",") {
+                valueOrEnumValueName(it as Any?).toString()
+            }
         } catch (exception: Exception) {
-            "= <${exception.message}>"
+            "<${exception.message}>"
         }
-        return String.format("%s (%s) %s", name, if (is1setOf()) "1setOf $tag" else "$tag", valuesString)
+        return String.format("%s (%s) = %s", name, if (is1setOf()) "1setOf $tag" else "$tag", valuesString)
     }
 
     fun logDetails() {
@@ -51,10 +83,6 @@ class IppAttribute<T> constructor(val name: String, val tag: IppTag, val values:
                 println(" $value")
             }
         }
-    }
-
-    companion object {
-        var supportTagForAttribute: Boolean = true
     }
 
 }
