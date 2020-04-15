6cde4d884c81613d5ee369ffe104e24abd04e09d
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-15 22:26:17 +0200

    fixed typos and added more operations

--- src/main/kotlin/de/gmuth/ipp/core/IppOperation.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppOperation.kt
@@ -29,11 +29,13 @@ enum class IppOperation(val code: Short) {
     CreateJobSubscription(0x0017),
     GetSubscriptionAttributes(0x0018),
     GetSubscriptions(0x0019),
-    RenewSubscritpion(0x001A),
+    RenewSubscription(0x001A),
     CancelSubscription(0x001B),
     GetNotifications(0x001C),
-    SendNotifications(0x001D),
-    GetPrinterSupportFiles(0x0021),
+    GetResourceAttributes(0x001E),
+    GetResources(0x0020),
+
+    // RFC 3998
     EnablePrinter(0x0022),
     DisablePrinter(0x0023),
     PausePrinterAfterCurrentJob(0x0024),
@@ -50,22 +52,27 @@ enum class IppOperation(val code: Short) {
     ResumeJob(0x002F),
     PromoteJob(0x0030),
     ScheduleJobAfter(0x0031),
-    // a few more are missing
+
+    // PWG 5100 not listed
 
     // CUPS operations
     CupsGetDefault(0x4001),
     CupsGetPrinters(0x4002),
-    CupsAddPrinter(0x4003),
+    CupsAddModifyPrinter(0x4003),
     CupsDeletePrinter(0x4004),
     CupsGetClasses(0x4005),
-    CuspAddClass(0x4006),
+    CuspAddModifyClass(0x4006),
     CupsDeleteClass(0x4007),
     CupsAcceptJobs(0x4008),
     CupsRejectJobs(0x4009),
     CupsSetDefault(0x400A),
     CupsGetDevices(0x400B),
     CupsGetPPDs(0x400C),
-    CupsMoveJob(0x400D);
+    CupsMoveJob(0x400D),
+    CupsAuthenticateJob(0x400E),
+    CupsGetPPD(0x400F),
+    CupsGetDocument(0x4027),
+    CupsCreateLocalPrinter(0x4028);
 
     override fun toString(): String = registeredName()
 
