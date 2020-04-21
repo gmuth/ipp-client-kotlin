76ddd81c02626a3e45b59a04c727c4adeb62a39b
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-21 21:55:06 +0200

    replaced codeMap with direct lookup

--- src/main/kotlin/de/gmuth/ipp/client/IppPrinterState.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppPrinterState.kt
@@ -1,29 +1,21 @@
 package de.gmuth.ipp.client
 
-import de.gmuth.ipp.core.IppException
-
 /**
  * Copyright (c) 2020 Gerhard Muth
  */
 
 // "printer-state": type1 enum [RFC8011]
-enum class IppPrinterState(val code: Int) {
+enum class IppPrinterState(val code: Int, private val registeredValue: String) {
 
-    Idle(3),
-    Processing(4),
-    Stopped(5);
+    Idle(3, "idle"),
+    Processing(4, "processing"),
+    Stopped(5, "stopped");
 
     // https://www.iana.org/assignments/ipp-registrations/ipp-registrations.xml#ipp-registrations-6
-    private fun registeredValue() = name
-            .replace("[A-Z]".toRegex()) { "-" + it.value.toLowerCase() }
-            .replace("^-".toRegex(), "")
-
-    override fun toString() = registeredValue()
+    override fun toString() = registeredValue
 
     companion object {
-        private val codeMap = values().associateBy(IppPrinterState::code)
-        fun fromInt(code: Int): IppPrinterState = codeMap[code]
-                ?: throw IppException(String.format("job state code '%02X' undefined", code))
-
+        fun fromInt(code: Int) = values().single { it.code == code }
     }
+
 }
