c982e279ac6f98e96ba24c5ac645ac3771485fb8
Gerhard Muth <gerhard.muth@gmx.de>
2020-04-28 10:09:42 +0200

    assert that color/mode value is supported by printer

--- src/main/kotlin/de/gmuth/ipp/client/IppColorMode.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppColorMode.kt
@@ -9,17 +9,20 @@ import de.gmuth.ipp.core.IppAttributesGroup
 import de.gmuth.ipp.core.IppException
 import de.gmuth.ipp.core.IppTag
 
-enum class IppColorMode : IppJobParameter {
-    Auto, Color, Monochrome;
+enum class IppColorMode(val value: String) : IppJobParameter {
+    Auto("auto"),
+    Color("color"),
+    Monochrome("monochrome");
 
     override fun toIppAttribute(printerAttributes: IppAttributesGroup): IppAttribute<String> {
         // use job-creation-attributes-supported? // 5100.11
-        val attributeName = when {
-            printerAttributes["print-color-mode-supported"] != null -> "print-color-mode" // 5100.14 ipp everywhere
-            printerAttributes["output-mode-supported"] != null -> "output-mode" // cups extension
+        val modeAttributeName = when {
+            printerAttributes.containsKey("print-color-mode-supported") -> "print-color-mode" // 5100.14 ipp everywhere
+            printerAttributes.containsKey("output-mode-supported") -> "output-mode" // cups extension
             else -> throw IppException("printer does not support 'output-mode' or 'print-color-mode'")
         }
-        return IppAttribute(attributeName, IppTag.Keyword, registeredValue(name))
+        printerAttributes.assertValueSupported("$modeAttributeName-supported", value)
+        return IppAttribute(modeAttributeName, IppTag.Keyword, value)
     }
 }
 
