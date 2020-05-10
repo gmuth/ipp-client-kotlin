b65a0f379716ccc851a2d6b81a80ae3c0b4076fb
Gerhard Muth <gerhard.muth@gmx.de>
2020-05-10 22:09:44 +0200

    from companion object to singleton object

--- src/main/kotlin/de/gmuth/ipp/client/IppJobAttributes.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppJobAttributes.kt
@@ -9,40 +9,38 @@ import de.gmuth.ipp.core.IppIntegerRange
 import de.gmuth.ipp.core.IppResolution
 import de.gmuth.ipp.core.IppTag
 
-class IppJobAttributes {
-    companion object {
+object IppJobAttributes {
 
-        fun attribute(name: String, vararg values: Any) =
-                IppAttribute(name, values.toList())
+    fun attribute(name: String, vararg values: Any) =
+            IppAttribute(name, values.toList())
 
-        fun documentFormat(value: String) =
-                IppAttribute("document-format", IppTag.MimeMediaType, value)
+    fun documentFormat(value: String) =
+            IppAttribute("document-format", IppTag.MimeMediaType, value)
 
-        fun media(value: String) =
-                IppAttribute("media", IppTag.Keyword, value)
+    fun media(value: String) =
+            IppAttribute("media", IppTag.Keyword, value)
 
-        fun jobName(value: String) =
-                IppAttribute("job-name", IppTag.NameWithoutLanguage, value)
+    fun jobName(value: String) =
+            IppAttribute("job-name", IppTag.NameWithoutLanguage, value)
 
-        fun jobPriority(value: Int) =
-                IppAttribute("job-priority", IppTag.Integer, value)
+    fun jobPriority(value: Int) =
+            IppAttribute("job-priority", IppTag.Integer, value)
 
-        fun copies(value: Int) =
-                IppAttribute("copies", IppTag.Integer, value)
+    fun copies(value: Int) =
+            IppAttribute("copies", IppTag.Integer, value)
 
-        fun numberUp(value: Int) =
-                IppAttribute("number-up", IppTag.Integer, value)
+    fun numberUp(value: Int) =
+            IppAttribute("number-up", IppTag.Integer, value)
 
-        fun printerResolutionDpi(value: Int) =
-                IppAttribute("printer-resolution", IppTag.Resolution, IppResolution(value))
+    fun printerResolutionDpi(value: Int) =
+            IppAttribute("printer-resolution", IppTag.Resolution, IppResolution(value))
 
-        fun pageRanges(vararg ranges: IntRange) =
-                IppAttribute(
-                        "page-ranges",
-                        IppTag.RangeOfInteger,
-                        ranges.map {
-                            IppIntegerRange(it.first, it.last)
-                        }.toList()
-                )
-    }
+    fun pageRanges(vararg ranges: IntRange) =
+            IppAttribute(
+                    "page-ranges",
+                    IppTag.RangeOfInteger,
+                    ranges.map {
+                        IppIntegerRange(it.first, it.last)
+                    }.toList()
+            )
 }
