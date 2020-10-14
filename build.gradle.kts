2fe622d0ea1856ed16cc47e7bde1632e663e5541
Gerhard Muth <gerhard.muth@gmx.de>
2020-10-14 10:55:00 +0200

    workflow: publish and sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,9 @@ publishing {
             url = uri("https://maven.pkg.github.com/gmuth/ipp-client-kotlin")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("GITHUB_ACTOR")
-                password = project.findProperty("gpr.token") as String? ?: System.getenv("GPR_TOKEN")
+                password = project.findProperty("gpr.token") as String? ?: System.getenv("GITHUB_TOKEN")
+                //username = project.findProperty("gpr.user") as String?
+                //password = project.findProperty("gpr.token") as String?
             }
         }
     }
