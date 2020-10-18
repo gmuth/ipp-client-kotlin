59aa06de1fc1b8423b636fa5b57f3eefb3be70a7
Gerhard Muth <gerhard.muth@gmx.de>
2020-10-18 20:05:17 +0200

    remove 'inputstream()', implement write(OutputStream)

--- src/main/kotlin/de/gmuth/ipp/core/IppRequest.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppRequest.kt
@@ -5,6 +5,7 @@ package de.gmuth.ipp.core
  */
 
 import java.io.InputStream
+import java.io.OutputStream
 import java.io.SequenceInputStream
 import java.nio.charset.Charset
 import java.util.*
@@ -19,25 +20,19 @@ open class IppRequest() : IppMessage() {
     val operation: IppOperation
         get() = IppOperation.fromShort(code ?: throw IppException("operation-code must not be null"))
 
-    val inputStream: InputStream
-        get() {
-            val encodedInputStream = try {
-                encodedInputStream()
-            } catch (exception: Exception) {
-                throw IppException("failed to encode ipp request", exception)
-            }
-            return if (documentInputStream == null) {
-                if (operation.requiresDocument())
-                    throw IppException("missing document for '$operation' operation")
-                else
-                    encodedInputStream
-            } else {
-                if (operation.requiresDocument())
-                    SequenceInputStream(encodedInputStream, documentInputStream)
-                else
-                    throw IppException("found unexpected document for '$operation' operation")
-            }
+    override fun write(outputStream: OutputStream) {
+        try {
+            super.write(outputStream)
+        } catch (exception: Exception) {
+            throw IppException("failed to encode ipp request", exception)
         }
+        when {
+            operation.requiresDocument() && documentInputStream == null -> throw IppException("missing document for '$operation' operation")
+            !operation.requiresDocument() && documentInputStream != null -> throw IppException("found unexpected document for '$operation' operation")
+        }
+        // append document data
+        if (documentInputStream != null) documentInputStream!!.copyTo(outputStream)
+    }
 
     constructor(
             version: String,
@@ -45,7 +40,6 @@ open class IppRequest() : IppMessage() {
             requestId: Int,
             charset: Charset = Charsets.UTF_8,
             naturalLanguage: Locale = Locale.ENGLISH
-
     ) : this() {
         this.version = version
         this.code = operationCode
