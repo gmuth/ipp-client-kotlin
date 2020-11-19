9bc73398059332cb75793865254df5f59ab6c3de
Gerhard Muth <gerhard.muth@gmx.de>
2020-11-20 00:20:46 +0100

    explain why printer attributes are provided here

--- src/main/kotlin/de/gmuth/ipp/core/IppAttributeBuilder.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppAttributeBuilder.kt
@@ -6,6 +6,7 @@ package de.gmuth.ipp.core
 
 interface IppAttributeBuilder {
 
+    // because some attribute names or values depend on the printers capabilities we provide the printer attributes here (see also IppColorMode).
     fun buildIppAttribute(printerAttributes: IppAttributesGroup): IppAttribute<*>
 
 }
