b125010f9c15a9996c03de5270a7ed42e19755f2
Gerhard Muth <gerhard.muth@gmx.de>
2020-12-14 00:00:38 +0100

    response test

---
+++ src/test/kotlin/de/gmuth/ipp/core/IppResponseTests.kt
@@ -0,0 +1,26 @@
+package de.gmuth.ipp.core
+
+/**
+ * Copyright (c) 2020 Gerhard Muth
+ */
+
+import java.io.File
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class IppResponseTests {
+
+    @Test
+    fun printJobResponse2() {
+        val response = IppResponse().apply { read(File("src/test/resources/printJob.response")) }
+        with(response) {
+            assertTrue(isSuccessful())
+            assertEquals(0, printerGroup.size)
+            assertEquals(0, jobGroup.size)
+            assertEquals("successful-ok", codeDescription)
+            assertEquals("not-infected", statusMessage.toString())
+        }
+    }
+
+}
