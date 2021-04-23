2d8d2b63c1535239929b781f84ea15324b99b837
Gerhard Muth <gerhard.muth@gmx.de>
2021-04-23 22:40:37 +0200

    refactored method signatures

--- src/main/kotlin/de/gmuth/log/Logging.kt
+++ src/main/kotlin/de/gmuth/log/Logging.kt
@@ -19,24 +19,25 @@ object Logging {
 
     open class Logger(val name: String, var logLevel: LogLevel = defaultLogLevel) {
 
-        fun trace(messageProducer: () -> Any?) = trace(null, messageProducer)
-        fun debug(messageProducer: () -> Any?) = debug(null, messageProducer)
-        fun info(messageProducer: () -> Any?) = info(null, messageProducer)
-        fun warn(messageProducer: () -> Any?) = warn(null, messageProducer)
-        fun error(messageProducer: () -> Any?) = error(null, messageProducer)
+        @JvmOverloads
+        fun trace(throwable: Throwable? = null, messageProducer: () -> Any?) = log(LogLevel.TRACE, throwable, messageProducer)
 
-        fun trace(throwable: Throwable?, messageProducer: () -> Any?) = log(LogLevel.TRACE, throwable, messageProducer)
-        fun debug(throwable: Throwable?, messageProducer: () -> Any?) = log(LogLevel.DEBUG, throwable, messageProducer)
-        fun info(throwable: Throwable?, messageProducer: () -> Any?) = log(LogLevel.INFO, throwable, messageProducer)
-        fun warn(throwable: Throwable?, messageProducer: () -> Any?) = log(LogLevel.WARN, throwable, messageProducer)
-        fun error(throwable: Throwable?, messageProducer: () -> Any?) = log(LogLevel.ERROR, throwable, messageProducer)
+        @JvmOverloads
+        fun debug(throwable: Throwable? = null, messageProducer: () -> Any?) = log(LogLevel.DEBUG, throwable, messageProducer)
 
-        fun log(messageLogLevel: LogLevel, throwable: Throwable?, messageProducer: () -> Any?) {
+        @JvmOverloads
+        fun info(throwable: Throwable? = null, messageProducer: () -> Any?) = log(LogLevel.INFO, throwable, messageProducer)
+
+        @JvmOverloads
+        fun warn(throwable: Throwable? = null, messageProducer: () -> Any?) = log(LogLevel.WARN, throwable, messageProducer)
+
+        @JvmOverloads
+        fun error(throwable: Throwable? = null, messageProducer: () -> Any?) = log(LogLevel.ERROR, throwable, messageProducer)
+
+        @JvmOverloads
+        fun log(messageLogLevel: LogLevel, throwable: Throwable? = null, messageProducer: () -> Any?) {
             if (isEnabled(messageLogLevel)) dispatch(messageLogLevel, throwable, messageProducer()?.toString() ?: "null")
         }
-
-        fun log(messageLogLevel: LogLevel, messageProducer: () -> Any?) =
-                log(messageLogLevel, null, messageProducer)
 
         open fun isEnabled(level: LogLevel) = logLevel <= level
 
@@ -54,6 +55,7 @@ object Logging {
 
     var factory = Factory { Logger(it) }
 
+    @JvmOverloads
     fun getLogger(name: String, logLevel: LogLevel = defaultLogLevel): Logger {
         if (debugLoggingConfig) println(String.format("new Logger: level=%-5s name=%s", logLevel, name))
         return factory.getLogger(name).apply { this.logLevel = logLevel }
