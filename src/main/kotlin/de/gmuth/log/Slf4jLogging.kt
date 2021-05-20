018c0f3f0125db78b155f80a08d870675f063b60
Gerhard Muth <gerhard.muth@gmx.de>
2021-05-20 17:01:31 +0200

    slf4j logger adapter

--- src/main/kotlin/de/gmuth/log/Slf4jLogging.kt
+++ src/main/kotlin/de/gmuth/log/Slf4jLogging.kt
@@ -9,36 +9,34 @@ import de.gmuth.log.Logging.LogLevel.*
 
 object Slf4jLogging {
 
-    class Logger(name: String, logLevel: LogLevel = Logging.defaultLogLevel) : Logging.Logger(name, logLevel) {
+    class Slf4jLoggerAdapter(name: String) : Logging.Logger(name) {
 
-        private val slf4jLogger: org.slf4j.Logger = org.slf4j.LoggerFactory.getLogger(name)
+        val slf4jLogger = org.slf4j.LoggerFactory.getLogger(name)
 
-        override fun isEnabled(level: Logging.LogLevel) =
-                with(slf4jLogger) {
-                    when (level) { // is enabled
-                        TRACE -> isTraceEnabled
-                        DEBUG -> isDebugEnabled
-                        INFO -> isInfoEnabled
-                        WARN -> isWarnEnabled
-                        ERROR -> isErrorEnabled
-                    }
-                }
+        override fun isEnabled(level: LogLevel) = with(slf4jLogger) {
+            when (level) {
+                TRACE -> isTraceEnabled
+                DEBUG -> isDebugEnabled
+                INFO -> isInfoEnabled
+                WARN -> isWarnEnabled
+                ERROR -> isErrorEnabled
+            }
+        }
 
-        override fun dispatch(messageLogLevel: LogLevel, throwable: Throwable?, messageString: String) =
-                with(slf4jLogger) {
-                    when (messageLogLevel) { // delegate to slf4j
-                        TRACE -> trace(messageString, throwable)
-                        DEBUG -> debug(messageString, throwable)
-                        INFO -> info(messageString, throwable)
-                        WARN -> warn(messageString, throwable)
-                        ERROR -> error(messageString, throwable)
-                    }
-                }
+        override fun dispatch(messageLogLevel: LogLevel, throwable: Throwable?, messageString: String) = with(slf4jLogger) {
+            when (messageLogLevel) {
+                TRACE -> trace(messageString, throwable)
+                DEBUG -> debug(messageString, throwable)
+                INFO -> info(messageString, throwable)
+                WARN -> warn(messageString, throwable)
+                ERROR -> error(messageString, throwable)
+            }
+        }
     }
 
     fun configure() {
         Logging.useSimpleClassName = false
-        Logging.factory = Logging.Factory { Logger(it) }
+        Logging.factory = Logging.Factory { Slf4jLoggerAdapter(it) }
     }
 
 }
