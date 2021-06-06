c1e56351ded991ea3525c777b0e906625049e141
Gerhard Muth <gerhard.muth@gmx.de>
2021-06-07 00:20:03 +0200

    removed interpretation of "time" or "time-out" integer values

--- src/main/kotlin/de/gmuth/ipp/core/IppAttribute.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppAttribute.kt
@@ -8,23 +8,17 @@ import de.gmuth.ipp.iana.IppRegistrationsSection2
 import de.gmuth.ipp.iana.IppRegistrationsSection6.Companion.getEnumName
 import de.gmuth.log.Logging
 import java.nio.charset.Charset
-import java.text.SimpleDateFormat
-import java.util.*
 
 data class IppAttribute<T> constructor(val name: String, val tag: IppTag) : IppAttributeBuilder {
 
     companion object {
         val log = Logging.getLogger {}
-        val iso8601DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
     }
 
     val values = mutableListOf<T>()
 
     init {
-        if (tag.isDelimiterTag()) {
-            log.error { "delimiter tag '$tag' must not be used for attributes" }
-            throw IllegalArgumentException("$tag")
-        }
+        if (tag.isDelimiterTag()) throw IllegalArgumentException("$tag")
     }
 
     constructor(name: String, tag: IppTag, values: Collection<T>) : this(name, tag) {
@@ -38,21 +32,17 @@ data class IppAttribute<T> constructor(val name: String, val tag: IppTag) : IppA
     constructor(name: String, tag: IppTag, vararg values: T) : this(name, tag, values.toList())
 
     val value: T
-        get() {
-            if (IppRegistrationsSection2.attributeIs1setOf(name) == true) {
-                log.warn { "'$name' is registered as '1setOf', use 'values' instead" }
-            }
-            return values.single()
+        get() = values.single().apply {
+            if (IppRegistrationsSection2.attributeIs1setOf(name) == true) log.warn { "'$name' is registered as '1setOf', use 'values' instead" }
         }
 
     @Suppress("UNCHECKED_CAST")
-    fun additionalValue(attribute: IppAttribute<*>) =
-            when {
-                attribute.tag != tag -> throw IppException("expected additional value with tag '$tag' but found $attribute")
-                attribute.values.size != 1 -> throw IppException("expected 1 additional value, not ${attribute.values.size}")
-                attribute.name.isNotEmpty() -> throw IppException("for additional '$name' values attribute name must be empty")
-                else -> values.add(attribute.value as T)
-            }
+    fun additionalValue(attribute: IppAttribute<*>) = when {
+        attribute.tag != tag -> throw IppException("expected additional value with tag '$tag' but found $attribute")
+        attribute.values.size != 1 -> throw IppException("expected 1 additional value, not ${attribute.values.size}")
+        attribute.name.isNotEmpty() -> throw IppException("for additional '$name' values attribute name must be empty")
+        else -> values.add(attribute.value as T)
+    }
 
     fun is1setOf() =
             values.size > 1 || IppRegistrationsSection2.attributeIs1setOf(name) == true
@@ -61,30 +51,15 @@ data class IppAttribute<T> constructor(val name: String, val tag: IppTag) : IppA
 
     override fun toString(): String {
         val tagString = "${if (is1setOf()) "1setOf " else ""}$tag"
-        val valuesString =
-                if (values.isEmpty()) "no-value"
-                else values.joinToString(",") { valueToString(it) }
+        val valuesString = if (values.isEmpty()) "no-value" else values.joinToString(",") { valueToString(it) }
         return "$name ($tagString) = $valuesString"
     }
 
-    private fun valueToString(value: T) = when {
-        tag == IppTag.Charset -> with(value as Charset) {
-            name().toLowerCase()
-        }
-        tag == IppTag.RangeOfInteger -> with(value as IntRange) {
-            "$start-$endInclusive"
-        }
-        tag == IppTag.Integer && name.contains("time") && !name.contains("time-out") -> with(value as Int) {
-            val seconds = value.toLong() // some printers use 'seconds since startup'
-            val epochSeconds = if (seconds < 60 * 60 * 24 * 365) Date().time / 1000 - seconds else seconds
-            "$value (${iso8601DateFormat.format(Date(epochSeconds * 1000))})"
-        }
-        value is ByteArray -> with(value as ByteArray) {
-            if (isEmpty()) "no-value" else "$size bytes"
-        }
-        else -> {
-            enumNameOrValue(value as Any).toString()
-        }
+    internal fun valueToString(value: T) = when {
+        tag == IppTag.Charset -> with(value as Charset) { name().toLowerCase() }
+        tag == IppTag.RangeOfInteger -> with(value as IntRange) { "$start-$endInclusive" }
+        value is ByteArray -> with(value as ByteArray) { if (isEmpty()) "no-value" else "$size bytes" }
+        else -> enumNameOrValue(value as Any).toString()
     }
 
     fun enumNameOrValue(value: Any) =
