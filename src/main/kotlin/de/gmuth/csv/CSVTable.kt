c4f7a3bbeb2f80ff99848d9edf9ece3ac88bf4a7
Gerhard Muth <gerhard.muth@gmx.de>
2021-07-02 22:28:15 +0200

    read cvs file into list of typed rows

---
+++ src/main/kotlin/de/gmuth/csv/CSVTable.kt
@@ -0,0 +1,96 @@
+package de.gmuth.csv
+
+/**
+ * Copyright (c) 2021 Gerhard Muth
+ */
+
+import java.io.InputStream
+
+// https://tools.ietf.org/html/rfc4180
+
+class CSVTable<T>(
+        inputStream: InputStream? = null,
+        val buildRow: (columns: List<String>) -> T,
+        val skipHeader: Boolean = true,
+) {
+
+    val rows: MutableList<T> = mutableListOf()
+    val maxLengthMap = mutableMapOf<Int, Int>()
+
+    init {
+        inputStream?.let { read(it) }
+    }
+
+    constructor(resourcePath: String, rowFactory: (columns: List<String>) -> T, skipHeader: Boolean = true) :
+            this(CSVTable.javaClass.getResourceAsStream(resourcePath), rowFactory, skipHeader)
+
+    fun updateMaxLengthMap(columnIndex: Int, columnLength: Int) =
+            with(maxLengthMap[columnIndex]) { if (this == null || this < columnLength) maxLengthMap[columnIndex] = columnLength }
+
+    fun read(inputStream: InputStream) {
+        if (skipHeader) parseRow(inputStream)
+        lineLoop@ while (true) {
+            val columns = parseRow(inputStream) ?: break@lineLoop
+            rows.add(buildRow(columns))
+        }
+    }
+
+    fun parseRow(inputStream: InputStream): List<String>? {
+        val fields = mutableListOf<String>()
+        var currentField = StringBuffer()
+        var inQuote = false
+        var lastCharacterWasQuote = false
+        fun updateMaxLengthMap() = updateMaxLengthMap(fields.size - 1, fields.last().length)
+        columnLoop@ while (true) {
+            val i = inputStream.read()
+            if (i == -1) break@columnLoop
+            val char = i.toChar()
+            var appendCharacter = false
+            if (inQuote) {
+                appendCharacter = char != '"'
+            } else {
+                when (char) {
+                    ',' -> {
+                        fields.add(currentField.toString())
+                        updateMaxLengthMap()
+                        currentField = StringBuffer()
+                    }
+                    '\n' -> {
+                        fields.add(currentField.toString())
+                        updateMaxLengthMap()
+                        return fields
+                    }
+                    '"' -> {
+                        appendCharacter = lastCharacterWasQuote
+                    }
+                    else -> {
+                        appendCharacter = char != '\r'
+                    }
+                }
+            }
+            lastCharacterWasQuote = char == '"'
+            if (lastCharacterWasQuote) inQuote = !inQuote
+            if (appendCharacter) currentField.append(char)
+        }
+        if (currentField.isEmpty()) return null
+        fields.add(currentField.toString())
+        updateMaxLengthMap()
+        return fields
+    }
+
+    companion object {
+
+        fun print(inputStream: InputStream, delimiter: String = "|") =
+                with(CSVTable(inputStream, buildRow = { columns: List<String> -> columns })) {
+                    for (columns in rows) with(StringBuffer(delimiter)) {
+                        for ((columnIndex, column) in columns.withIndex()) {
+                            append("%-${maxLengthMap[columnIndex]}s%s".format(column, delimiter))
+                        }
+                        println(toString())
+                    }
+                }
+
+        fun print(resourcePath: String, delimiter: String = "|") = print(javaClass.getResourceAsStream(resourcePath), delimiter)
+
+    }
+}
