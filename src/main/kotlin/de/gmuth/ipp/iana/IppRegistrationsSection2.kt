7d15ea1463828089c690fe4051df863550151215
Gerhard Muth <gerhard.muth@gmx.de>
2021-07-03 23:34:20 +0200

    refactored validation, support validation of collections

--- src/main/kotlin/de/gmuth/ipp/iana/IppRegistrationsSection2.kt
+++ src/main/kotlin/de/gmuth/ipp/iana/IppRegistrationsSection2.kt
@@ -5,6 +5,9 @@ package de.gmuth.ipp.iana
  */
 
 import de.gmuth.csv.CSVTable
+import de.gmuth.ipp.core.IppAttribute
+import de.gmuth.ipp.core.IppCollection
+import de.gmuth.ipp.core.IppMessage
 import de.gmuth.ipp.core.IppTag
 import de.gmuth.log.Logging
 
@@ -77,18 +80,84 @@ object IppRegistrationsSection2 {
 
     val attributesMap = allAttributes.associateBy(Attribute::key)
 
-    fun tagForAttribute(name: String) = attributesMap[name]?.tag()
-
-    fun attributeIs1setOf(name: String) = attributesMap[name]?.is1setOf()
-
-    fun checkSyntaxOfAttribute(name: String, tag: IppTag) {
-        if (tag.isOutOfBandTag()) return
-        val syntax = attributesMap[name]?.syntax
-        if (syntax != null && syntax.isNotEmpty() && !syntax.contains(tag.registeredSyntax())) {
-            log.warn { "$name ($tag) does not match syntax '$syntax'" }
-        }
+    //  alias example: Printer Description,media-col-default,"<Member attributes are the same as the ""media-col"" Job Template attribute>"
+    val aliasMap = mutableMapOf<String, String>().apply {
+        allAttributes
+                .filter { it.memberAttribute.toLowerCase().contains("same as") }
+                .forEach { put(it.name, it.memberAttribute.replace("^.*\"(.+)\".*$".toRegex(), "$1")) }
+        // apple cups extension 'output-mode' was standardized to 'print-color-mode'
+        put("output-mode-default", "print-color-mode-default")
+        put("output-mode-supported", "print-color-mode-supported")
+        // 'media-col-default' resolves to 'media-col' and 'media-source-feed-...' values are registered for 'media-col-ready'
+        put("media-col/media-source-properties/media-source-feed-direction", "media-col-ready/media-source-properties/media-source-feed-direction")
+        put("media-col/media-source-properties/media-source-feed-orientation", "media-col-ready/media-source-properties/media-source-feed-orientation")
     }
+
+    fun resolveAlias(name: String) = (aliasMap[name] ?: name).also {
+        if (aliasMap.containsKey(name)) log.trace { "'$name' resolves to '$it'" }
+    }
+
+    fun getAttribute(name: String, resolveAlias: Boolean = true) = attributesMap[if (resolveAlias) resolveAlias(name) else name]
+
+    fun syntaxForAttribute(name: String, resolveAlias: Boolean) = getAttribute(name, resolveAlias)?.syntax
+
+    fun tagForAttribute(name: String) = getAttribute(name)?.tag()
+
+    fun attributeIs1setOf(name: String) = getAttribute(name, false)?.is1setOf()
 
     fun selectGroupForAttribute(name: String) = attributesMap[name]?.collectionGroupTag() ?: IppTag.Job
 
+    val unknownAttributes = mutableSetOf<String>()
+
+    fun checkSyntaxOfAttribute(name: String, tag: IppTag) {
+        if (tag.isOutOfBandTag()) return
+        val syntax = syntaxForAttribute(name, true)
+        if (syntax != null && !syntax.contains(tag.registeredSyntax())) {
+            log.warn { "$name ($tag) does not match syntax '$syntax'" }
+        }
+        if (syntax == null) {
+            log.trace { "no syntax found for '$name'" }
+            unknownAttributes.add(name)
+        }
+    }
+
+    fun validate(ippMessage: IppMessage) {
+        for (group in ippMessage.attributesGroups) {
+            for (attribute in group.values) {
+                validate(attribute)
+            }
+        }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun validate(ippAttribute: IppAttribute<*>) = with(ippAttribute) {
+        checkSyntaxOfAttribute(name, tag)
+        if (isCollection()) validate(name, values as List<IppCollection>)
+        if (!tag.isOutOfBandTag() && values.isEmpty()) log.warn { "'$name' ($tag) has no values" }
+        if (values.size > 1 && attributeIs1setOf(name) == false) log.warn { "'$name' is not registered as '1setOf'" }
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun validate(name: String, ippCollections: List<IppCollection>) {
+        log.trace { "validate collection '$name'" }
+        for (ippCollection in ippCollections) {
+            log.trace { "         ${ippCollection.members.size} members" }
+            val resolvedName = resolveAlias(name)
+            for (member in ippCollection.members) {
+                if (member.isCollection()) {
+                    validate("$resolvedName/${member.name}", member.values as List<IppCollection>)
+                } else {
+                    checkSyntaxOfAttribute("$resolvedName/${member.name}", member.tag)
+                }
+            }
+        }
+    }
+
+    fun logUnknownAttributes() {
+        with(unknownAttributes.toMutableList()) {
+            sort()
+            log.info { "$size unknown attributes:" }
+            forEach { log.info { "- $it" } }
+        }
+    }
 }
