33343809ba4a828889b3968b62d7df3865c7f266
Gerhard Muth <gerhard.muth@gmx.de>
2021-07-22 20:32:14 +0200

    refactored

--- src/main/kotlin/de/gmuth/io/ByteArraySavingInputStream.kt
+++ src/main/kotlin/de/gmuth/io/ByteArraySavingInputStream.kt
@@ -7,15 +7,13 @@ import java.io.InputStream
  * Copyright (c) 2020-2021 Gerhard Muth
  */
 
-class ByteArraySavingInputStream(val inputStream: InputStream) : InputStream() {
+class ByteArraySavingInputStream(private val inputStream: InputStream) : InputStream() {
 
     var saveBytes: Boolean = true
     private val byteArrayOutputStream = ByteArrayOutputStream()
 
-    override fun read(): Int {
-        val byte = inputStream.read()
-        if (byte != -1 && saveBytes) byteArrayOutputStream.write(byte)
-        return byte
+    override fun read() = inputStream.read().also {
+        if (saveBytes && it != -1) byteArrayOutputStream.write(it)
     }
 
     override fun close() {
--- src/main/kotlin/de/gmuth/io/ByteArraySavingOutputStream.kt
+++ src/main/kotlin/de/gmuth/io/ByteArraySavingOutputStream.kt
@@ -7,13 +7,12 @@ import java.io.OutputStream
  * Copyright (c) 2020-2021 Gerhard Muth
  */
 
-class ByteArraySavingOutputStream(val outputStream: OutputStream) : OutputStream() {
+class ByteArraySavingOutputStream(private val outputStream: OutputStream) : OutputStream() {
 
     var saveBytes: Boolean = true
     private val byteArrayOutputStream = ByteArrayOutputStream()
 
-    override fun write(byte: Int) {
-        outputStream.write(byte)
+    override fun write(byte: Int) = outputStream.write(byte).also {
         if (saveBytes) byteArrayOutputStream.write(byte)
     }
 
