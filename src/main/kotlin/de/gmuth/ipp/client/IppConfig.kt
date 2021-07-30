49b8b878ce8cb5806cb54f09ae6346743783069e
Gerhard Muth <gerhard.muth@gmx.de>
2021-07-30 10:24:38 +0200

    IppConfig for IppClient

---
+++ src/main/kotlin/de/gmuth/ipp/client/IppConfig.kt
@@ -0,0 +1,38 @@
+package de.gmuth.ipp.client
+
+import de.gmuth.http.Http
+import de.gmuth.log.Logging
+import java.nio.charset.Charset
+import javax.net.ssl.SSLSocketFactory
+
+class IppConfig(
+        timeout: Int = 30000, // milli seconds
+        userAgent: String? = "ipp-client-kotlin/2.1",
+        sslSocketFactory: SSLSocketFactory? = null,
+        verifySSLHostname: Boolean = false,
+        var httpBasicAuth: Http.BasicAuth? = null,
+        var userName: String? = if (httpBasicAuth != null) httpBasicAuth.user else System.getProperty("user.name"),
+        var ippVersion: String = "1.1",
+        var charset: Charset = Charsets.UTF_8,
+        var naturalLanguage: String = "en"
+
+) : Http.Config(
+        timeout,
+        userAgent,
+        sslSocketFactory,
+        verifySSLHostname
+) {
+    companion object {
+        val log = Logging.getLogger {}
+    }
+
+    fun logDetails() {
+        log.info { "timeout: $timeout" }
+        log.info { "userAgent: $userAgent" }
+        log.info { "verifySSLHostname: $verifySSLHostname" }
+        log.info { "userName: $userName" }
+        log.info { "ippVersion: $ippVersion" }
+        log.info { "charset: $charset" }
+        log.info { "naturalLanguage: $naturalLanguage" }
+    }
+}
