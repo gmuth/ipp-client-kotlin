4d5d339addb729c550db055588ad56ba261a0f64
Gerhard Muth <gerhard.muth@gmx.de>
2021-08-20 23:26:14 +0200

    refactored cups client, lazy fetch all printers

--- src/main/kotlin/de/gmuth/ipp/client/CupsClient.kt
+++ src/main/kotlin/de/gmuth/ipp/client/CupsClient.kt
@@ -4,23 +4,42 @@ package de.gmuth.ipp.client
  * Copyright (c) 2020-2021 Gerhard Muth
  */
 
+import de.gmuth.ipp.core.IppException
 import de.gmuth.ipp.core.IppOperation
 import de.gmuth.ipp.core.IppOperation.*
 import de.gmuth.ipp.core.IppTag.Printer
+import de.gmuth.log.Logging
 import java.net.URI
 
 // https://www.cups.org/doc/spec-ipp.html
 open class CupsClient(val cupsUri: URI) : IppClient() {
 
-    constructor(host: String = "localhost", port: Int = 631) :
-            this(URI.create(String.format("ipp://%s:%d", host, port)))
+    constructor(host: String = "localhost", port: Int = 631) : this(URI.create("ipp://$host:$port"))
 
-    // for convenient java usage
+    // for java
     constructor(host: String) : this(host, 631)
+
+    companion object {
+        val log = Logging.getLogger { }
+    }
 
     init {
         if (cupsUri.scheme == "ipps") config.trustAnyCertificate()
     }
+
+    val printerMap: Map<String, IppPrinter> by lazy {
+        exchangeSuccessfulIppRequest(CupsGetPrinters)
+                .getAttributesGroups(Printer)
+                .map { IppPrinter(it, this) }
+                .associateBy { it.name.text }
+    }
+
+    fun getPrinters() = printerMap.values
+
+    fun getPrinter(name: String) =
+            printerMap[name] ?: throw IppException("no such cups printer: $name").also {
+                log.warn { "available printers: ${printerMap.keys.joinToString(",")}" }
+            }
 
     protected fun ippRequest(operation: IppOperation) =
             ippRequest(operation, cupsUri)
@@ -33,13 +52,5 @@ open class CupsClient(val cupsUri: URI) : IppClient() {
 
     fun getDefault() =
             IppPrinter(exchangeSuccessfulIppRequest(CupsGetDefault).printerGroup, this)
-
-    fun getPrinters() =
-            exchangeSuccessfulIppRequest(CupsGetPrinters)
-                    .getAttributesGroups(Printer)
-                    .map { IppPrinter(it, this) }
-
-    fun getPrinter(name: String) =
-            getPrinters().find { it.name.text == name } ?: throw NoSuchElementException(name)
 
 }
