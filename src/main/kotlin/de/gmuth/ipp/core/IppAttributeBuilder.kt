197aa6d20742f5f20260e0e6b07d4d07ce2a0fd3
Gerhard Muth <gerhard.muth@gmx.de>
2021-08-20 11:50:45 +0200

    comment format

--- src/main/kotlin/de/gmuth/ipp/core/IppAttributeBuilder.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppAttributeBuilder.kt
@@ -6,7 +6,8 @@ package de.gmuth.ipp.core
 
 interface IppAttributeBuilder {
 
-    // because some attribute names or values depend on the printers capabilities we provide the printer attributes here (see also IppColorMode).
+    // because some attribute names or values depend on the printers capabilities
+    // we provide the printer attributes here (see also IppColorMode).
     fun buildIppAttribute(printerAttributes: IppAttributesGroup): IppAttribute<*>
 
 }
