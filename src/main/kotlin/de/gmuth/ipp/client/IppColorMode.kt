727fdde10fb303cd07960de54e63ab058cbb1f5c
Gerhard Muth <gerhard.muth@gmx.de>
2021-08-23 20:08:47 +0200

    try 'printer-color-mode' as default when no printer attributes are available

--- src/main/kotlin/de/gmuth/ipp/client/IppColorMode.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppColorMode.kt
@@ -7,8 +7,8 @@ package de.gmuth.ipp.client
 import de.gmuth.ipp.core.IppAttribute
 import de.gmuth.ipp.core.IppAttributeBuilder
 import de.gmuth.ipp.core.IppAttributesGroup
-import de.gmuth.ipp.core.IppException
 import de.gmuth.ipp.core.IppTag.Keyword
+import de.gmuth.log.Logging
 
 enum class IppColorMode(private val keyword: String) : IppAttributeBuilder {
 
@@ -16,12 +16,20 @@ enum class IppColorMode(private val keyword: String) : IppAttributeBuilder {
     Color("color"),
     Monochrome("monochrome");
 
+    companion object {
+        val log = Logging.getLogger {}
+    }
+
     override fun buildIppAttribute(printerAttributes: IppAttributesGroup): IppAttribute<String> {
         // use job-creation-attributes-supported? // 5100.11
         val modeAttributeName = when {
             printerAttributes.containsKey("print-color-mode-supported") -> "print-color-mode" // 5100.14 ipp everywhere
             printerAttributes.containsKey("output-mode-supported") -> "output-mode" // cups extension
-            else -> throw IppException("printer does not support 'output-mode' or 'print-color-mode'")
+            else -> {
+                if (printerAttributes.isEmpty()) log.warn { "no printer attributes" }
+                else log.warn { "printer does not support 'output-mode' or 'print-color-mode'" }
+                "print-color-mode"
+            }
         }
         return IppAttribute(modeAttributeName, Keyword, keyword)
     }
