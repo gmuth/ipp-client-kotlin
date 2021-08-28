f7a2db228e86e4cd31a070e3651b18a0b29132e2
Gerhard Muth <gerhard.muth@gmx.de>
2021-08-28 22:23:19 +0200

    reformatted source code

--- src/main/kotlin/de/gmuth/io/ByteArrayExtension.kt
+++ src/main/kotlin/de/gmuth/io/ByteArrayExtension.kt
@@ -1,14 +1,13 @@
 package de.gmuth.io
 
-fun ByteArray.hexdump(maxRows: Int = 32, dump: (String) -> Unit) = with(StringBuilder()) {
-    for ((index, b) in withIndex()) {
-        append("%02X ".format(b))
-        if ((index + 1) % maxRows == 0) {
-            dump(toString())
-            clear()
+fun ByteArray.hexdump(maxRows: Int = 32, dump: (String) -> Unit) =
+        with(StringBuilder()) {
+            for ((index, b) in withIndex()) {
+                append("%02X ".format(b))
+                if ((index + 1) % maxRows == 0) {
+                    dump(toString())
+                    clear()
+                }
+            }
+            if (isNotEmpty()) dump(toString())
         }
-    }
-    if (isNotEmpty()) {
-        dump(toString())
-    }
-}
