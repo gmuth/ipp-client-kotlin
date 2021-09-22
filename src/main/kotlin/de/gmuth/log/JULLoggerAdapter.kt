953b906291d5772ba549cb53bac36eab7569e35a
Gerhard Muth <gerhard.muth@gmx.de>
2021-09-22 20:19:04 +0200

    refactored adapter, toJULLevel()

--- src/main/kotlin/de/gmuth/log/JULLoggerAdapter.kt
+++ src/main/kotlin/de/gmuth/log/JULLoggerAdapter.kt
@@ -12,24 +12,18 @@ class JULLoggerAdapter(name: String) : Logging.Logger(name) {
 
     val julLogger = java.util.logging.Logger.getLogger(name)
 
-    override fun isEnabled(level: LogLevel) = with(julLogger) {
-        when (level) {
-            TRACE -> isLoggable(Level.FINER)
-            DEBUG -> isLoggable(Level.FINE)
-            INFO -> isLoggable(Level.INFO)
-            WARN -> isLoggable(Level.WARNING)
-            ERROR -> isLoggable(Level.SEVERE)
-        }
+    private fun toJULLevel(logLevel: LogLevel) = when (logLevel) {
+        TRACE -> Level.FINER
+        DEBUG -> Level.FINE
+        INFO -> Level.INFO
+        WARN -> Level.WARNING
+        ERROR -> Level.SEVERE
     }
 
-    override fun publish(messageLogLevel: LogLevel, throwable: Throwable?, messageString: String?) = with(julLogger) {
-        when (messageLogLevel) {
-            TRACE -> log(Level.FINER, messageString, throwable)
-            DEBUG -> log(Level.FINE, messageString, throwable)
-            INFO -> log(Level.INFO, messageString, throwable)
-            WARN -> log(Level.WARNING, messageString, throwable)
-            ERROR -> log(Level.SEVERE, messageString, throwable)
-        }
-    }
+    override fun isEnabled(logLevel: LogLevel) =
+            julLogger.isLoggable(toJULLevel(logLevel))
+
+    override fun publish(messageLogLevel: LogLevel, throwable: Throwable?, messageString: String?) =
+            julLogger.log(toJULLevel(messageLogLevel), messageString, throwable)
 
 }
