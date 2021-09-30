6bd2b8db61cd9abb1f411c2285be4d367c1ae67a
Gerhard Muth <gerhard.muth@gmx.de>
2021-09-30 23:50:28 +0200

    CupsClient tests

---
+++ src/test/kotlin/de/gmuth/ipp/client/CupsClientTests.kt
@@ -0,0 +1,71 @@
+package de.gmuth.ipp.client
+
+/**
+ * Copyright (c) 2020-2021 Gerhard Muth
+ */
+
+import de.gmuth.http.HttpClientMock
+import de.gmuth.ipp.core.IppException
+import de.gmuth.ipp.core.IppResponse
+import de.gmuth.ipp.core.IppStatus.ClientErrorNotFound
+import de.gmuth.ipp.core.IppStatus.SuccessfulOk
+import de.gmuth.log.Logging
+import org.junit.Test
+import java.net.URI
+import kotlin.test.assertFailsWith
+import kotlin.test.assertTrue
+
+class CupsClientTests {
+
+    companion object {
+        val log = Logging.getLogger { }
+    }
+
+    val httpClient = HttpClientMock()
+    val cupsClient = CupsClient(URI.create("ipps://cups"), httpClient = httpClient)
+
+    init {
+        httpClient.mockResponse("CUPS/Cups-Get-Printers.ipp")
+        log.info { "${cupsClient.printerMap.size} cups printers" }
+        httpClient.ippResponse = IppResponse(SuccessfulOk) // default mocked response
+    }
+
+    @Test
+    fun constructors() {
+        CupsClient()
+        CupsClient("host")
+        CupsClient(config = IppConfig())
+    }
+
+    @Test
+    fun getPrinters() {
+        cupsClient.getPrinters().forEach { log.info { it } }
+    }
+
+    @Test
+    fun getPrinter() {
+        cupsClient.getPrinter("ColorJet_HP")
+    }
+
+    @Test
+    fun getPrinterFails() {
+        assertFailsWith<IppException> { // no such cups printer
+            cupsClient.getPrinter("invalid")
+        }
+    }
+
+    @Test
+    fun getDefaultFails() {
+        val exception = assertFailsWith<IppExchangeException> {
+            httpClient.mockResponse("CUPS/Cups-Get-Default-Error.ipp")
+            cupsClient.getDefault()
+        }
+        assertTrue(exception.statusIs(ClientErrorNotFound))
+    }
+
+    @Test
+    fun setDefault() {
+        cupsClient.setDefault(URI.create("ipp://cups:631/printers/matrix"))
+    }
+
+}
