54b17bd88ab2f5ba7155ce67796887718b53ce21
Gerhard Muth <gerhard.muth@gmx.de>
2022-03-13 21:32:54 +0100

    logAllMessages()

--- src/main/kotlin/de/gmuth/ipp/core/IppException.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppException.kt
@@ -1,8 +1,22 @@
 package de.gmuth.ipp.core
 
+import de.gmuth.log.Logging
+import de.gmuth.log.Logging.LogLevel.ERROR
+
 /**
- * Copyright (c) 2020-2021 Gerhard Muth
+ * Copyright (c) 2020-2022 Gerhard Muth
  */
 
-open class IppException(message: String, cause: Throwable? = null) :
-        RuntimeException(message, cause)
+open class IppException(message: String, cause: Throwable? = null) : RuntimeException(message, cause) {
+
+    companion object {
+        val log = Logging.getLogger {}
+        fun logAllMessages(throwable: Throwable, logLevel: Logging.LogLevel = ERROR) {
+            throwable.cause?.let { logAllMessages(it, logLevel) }
+            log.log(logLevel) { "${throwable.message}" }
+        }
+    }
+
+    fun logAllMessages(logLevel: Logging.LogLevel = ERROR) = logAllMessages(this, logLevel)
+
+}
