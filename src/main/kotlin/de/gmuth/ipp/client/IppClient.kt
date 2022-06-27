5c9bd37095ca507b1c4377f334e9d7d6ad272ab2
Gerhard Muth <gerhard.muth@gmx.de>
2022-06-27 22:52:45 +0200

    - log version and build - exchange: option to disable throwing exception when reponse status is not successful

--- src/main/kotlin/de/gmuth/ipp/client/IppClient.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppClient.kt
@@ -1,7 +1,7 @@
 package de.gmuth.ipp.client
 
 /**
- * Copyright (c) 2020-2021 Gerhard Muth
+ * Copyright (c) 2020-2022 Gerhard Muth
  */
 
 import de.gmuth.http.Http
@@ -21,8 +21,8 @@ import java.util.concurrent.atomic.AtomicInteger
 typealias IppResponseInterceptor = (request: IppRequest, response: IppResponse) -> Unit
 
 open class IppClient(
-        val config: IppConfig = IppConfig(),
-        val httpClient: Http.Client = Http.defaultImplementation.createClient(Http.Config())
+    val config: IppConfig = IppConfig(),
+    val httpClient: Http.Client = Http.defaultImplementation.createClient(Http.Config())
 ) {
     var saveMessages: Boolean = false
     var saveMessagesDirectory = File("ipp-messages")
@@ -38,11 +38,17 @@ open class IppClient(
     companion object {
         val log = Logging.getLogger {}
         const val APPLICATION_IPP = "application/ipp"
+        const val version = "2.3"
+        const val build = "20220627"
+
+        init {
+            println("IPP-Client: Version: $version, Build: $build, MIT License, (c) 2020-2022 Gerhard Muth")
+        }
     }
 
     init {
         httpConfig.apply {
-            userAgent = "ipp-client-kotlin/2.2"
+            userAgent = "ipp-client/$version"
             acceptEncoding = "identity" // avoids 'gzip' with Android's OkHttp
             accept = APPLICATION_IPP // avoids 'text/html' with sun.net.www.protocol.http.HttpURLConnection
         }
@@ -55,27 +61,27 @@ open class IppClient(
     private val requestCounter = AtomicInteger(1)
 
     fun ippRequest(
-            operation: IppOperation,
-            printerUri: URI,
-            jobId: Int? = null,
-            requestedAttributes: List<String>? = null
+        operation: IppOperation,
+        printerUri: URI,
+        jobId: Int? = null,
+        requestedAttributes: List<String>? = null
     ) = IppRequest(
-            operation,
-            printerUri,
-            jobId,
-            requestedAttributes,
-            config.userName,
-            config.ippVersion,
-            requestCounter.getAndIncrement(),
-            config.charset,
-            config.naturalLanguage
+        operation,
+        printerUri,
+        jobId,
+        requestedAttributes,
+        config.userName,
+        config.ippVersion,
+        requestCounter.getAndIncrement(),
+        config.charset,
+        config.naturalLanguage
     )
 
     //------------------------------------
     // exchange IppRequest for IppResponse
     //------------------------------------
 
-    open fun exchange(request: IppRequest): IppResponse {
+    open fun exchange(request: IppRequest, throwWhenNotSuccessful: Boolean = true): IppResponse {
         val ippUri: URI = request.printerUri
         val httpUri = toHttpUri(ippUri)
         log.trace { "send '${request.operation}' request to $ippUri" }
@@ -88,6 +94,7 @@ open class IppClient(
             val messageSubDirectory = File(saveMessagesDirectory, ippUri.host).apply {
                 if (!mkdirs() && !isDirectory) throw IppException("failed to create directory: $path")
             }
+
             fun file(suffix: String) = File(messageSubDirectory, "${request.requestId}-${request.operation}.$suffix")
             request.saveRawBytes(file("request"))
             response.saveRawBytes(file("response"))
@@ -95,13 +102,11 @@ open class IppClient(
 
         responseInterceptor?.invoke(request, response)
 
-        return response.apply {
-            if (!isSuccessful()) {
-                IppRegistrationsSection2.validate(request)
-                throw IppExchangeException(request, response)
-            }
-            if (status != SuccessfulOk) log.warn { "status: $status" }
+        if (!response.isSuccessful()) {
+            IppRegistrationsSection2.validate(request)
+            if (throwWhenNotSuccessful) throw IppExchangeException(request, response)
         }
+        return response
     }
 
     fun toHttpUri(ippUri: URI) = with(ippUri) {
@@ -111,9 +116,9 @@ open class IppClient(
     }
 
     fun httpPostRequest(httpUri: URI, request: IppRequest) = httpClient.post(
-            httpUri, APPLICATION_IPP,
-            { httpPostStream -> request.write(httpPostStream) },
-            chunked = request.hasDocument()
+        httpUri, APPLICATION_IPP,
+        { httpPostStream -> request.write(httpPostStream) },
+        chunked = request.hasDocument()
     ).apply {
         when { // http response is not successful
             !isOK() -> "http request to $httpUri failed: status=$status, content-type=$contentType${textContent()}"
@@ -133,15 +138,16 @@ open class IppClient(
             read(httpResponse.contentStream!!)
         } catch (exception: Exception) {
             throw IppExchangeException(
-                    request, this, httpResponse.status, "failed to decode ipp response", exception
+                request, this, httpResponse.status, "failed to decode ipp response", exception
             ).apply {
                 saveMessages("decoding_ipp_response_${request.requestId}_failed")
             }
         }
         if (status == ClientErrorBadRequest) request.logDetails("BAD-REQUEST: ")
+        if (status != SuccessfulOk) log.warn { "status: $status" }
         if (hasStatusMessage()) log.debug { "status-message: $statusMessage" }
         if (containsGroup(Unsupported)) unsupportedGroup.values.forEach {
-            log.warn { "unsupported attribute: $it" }
+            log.warn { "unsupported: $it" }
         }
     }
 }
