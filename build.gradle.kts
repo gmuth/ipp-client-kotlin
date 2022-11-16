962d2a4a2f3c3d60a2c9e484a6e2e84ed0dbde66
gmuth <gerhard.muth.ext@ausy-technologies.de>
2022-11-17 00:18:41 +0100

    upgrade kotlin to 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.dokka.gradle.DokkaTask
 // where is the jar? build/lib/ipp-client-kotlin...jar
 
 plugins {
-    id("org.jetbrains.kotlin.jvm") version "1.5.32"
+    id("org.jetbrains.kotlin.jvm") version "1.6.21"
     id("org.jetbrains.dokka") version "1.7.0"
     id("org.sonarqube") version "3.5.0.2730" // https://plugins.gradle.org/plugin/org.sonarqube
     id("maven-publish")
