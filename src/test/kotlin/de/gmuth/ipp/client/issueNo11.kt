17b4307fb653d544a723fc561dd76b993b5d378b
gmuth <gerhard.muth.ext@ausy-technologies.de>
2022-11-27 08:17:16 +0100

    removed comments

--- src/test/kotlin/de/gmuth/ipp/client/issueNo11.kt
+++ src/test/kotlin/de/gmuth/ipp/client/issueNo11.kt
@@ -12,23 +12,20 @@ fun main() {
 
     val log = Logging.getLogger {}
 
-    val printerUri = URI.create("ipp://192.168.31.244:631/ipp/print")
-    //val printerUri = URI.create("ipp://xero.local/ipp/print")
+    //val printerUri = URI.create("ipp://192.168.31.244:631/ipp/print")
+    val printerUri = URI.create("ipp://xero.local/ipp/print")
 
     try {
         log.info { "open ipp connection to $printerUri" }
         with(
             IppPrinter(
                 printerUri,
-                // omit accept and accept-encoding (null!)
                 httpConfig = Http.Config(debugLogging = true),
                 //getPrinterAttributesOnInit = false
             )
         ) {
-            //ippClient.httpConfig.debugLogging = true
-            //updateAttributes()
             log.info { "successfully connected $printerUri" }
-            //logDetails()
+            log.info { this }
             markers.forEach { log.info { it } }
         }
     } catch (exception: Exception) {
