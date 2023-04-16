34b5257f661ae024b7c3e9b04832a457e85e8362
gmuth <gerhard.muth.ext@ausy-technologies.de>
2023-04-16 13:12:09 +0200

    bugfix for cupsPrinterUri (-1 port)

--- src/main/kotlin/de/gmuth/ipp/client/CupsClient.kt
+++ src/main/kotlin/de/gmuth/ipp/client/CupsClient.kt
@@ -5,9 +5,12 @@ package de.gmuth.ipp.client
  */
 
 import de.gmuth.http.Http
-import de.gmuth.ipp.core.*
+import de.gmuth.ipp.core.IppOperation
 import de.gmuth.ipp.core.IppOperation.*
+import de.gmuth.ipp.core.IppRequest
+import de.gmuth.ipp.core.IppTag
 import de.gmuth.ipp.core.IppTag.*
+import de.gmuth.ipp.core.toIppString
 import de.gmuth.log.Logging
 import java.io.File
 import java.io.InputStream
@@ -17,7 +20,7 @@ import java.time.Duration
 // https://www.cups.org/doc/spec-ipp.html
 open class CupsClient(
     val cupsUri: URI = URI.create("ipp://localhost"),
-    val ippConfig: IppConfig = IppConfig(),
+    ippConfig: IppConfig = IppConfig(),
     httpClient: Http.Client = Http.defaultImplementation.createClient(Http.Config())
 ) {
     constructor(host: String = "localhost") : this(URI.create("ipp://$host"))
@@ -33,13 +36,9 @@ open class CupsClient(
         }
     }
 
-    val ippClient = IppClient(ippConfig, httpClient)
-
-    var userName: String?
-        get() = ippConfig.userName
-        set(value) {
-            ippConfig.userName = value
-        }
+    protected val ippClient = IppClient(ippConfig, httpClient)
+    var userName: String? by ippClient.config::userName
+    fun getIppServer() = ippClient.getHttpServer()
 
     fun getPrinters() = try {
         exchange(ippRequest(CupsGetPrinters))
@@ -62,32 +61,42 @@ open class CupsClient(
             throw exception
         }
 
-    fun getDefault() =
-        IppPrinter(exchange(ippRequest(CupsGetDefault)).printerGroup, ippClient)
+    fun getDefault() = IppPrinter(
+        exchange(ippRequest(CupsGetDefault)).printerGroup, ippClient
+    )
 
     fun setDefault(printerName: String) =
-        exchange(ippRequest(CupsSetDefault, cupsPrinterUri(printerName)))
+        exchangeCupsPrinterRequest(CupsSetDefault, printerName)
 
-    fun cupsPrinterUri(printerName: String) =
-        with(cupsUri) { URI("$scheme://$host:$port/printers/$printerName") }
+    fun cupsPrinterUri(printerName: String) = with(cupsUri) {
+        val optionalPort = if (port > 0) ":$port" else ""
+        URI("$scheme://$host$optionalPort/printers/$printerName")
+    }.apply {
+        log.debug { "cupsPrinterUri($printerName) -> $this" }
+    }
 
     // https://www.cups.org/doc/spec-ipp.html#CUPS_ADD_MODIFY_PRINTER
     fun addModifyPrinter(
         printerName: String,
-        deviceUri: URI,
-        printerInfo: String?,
-        printerLocation: String?,
+        deviceUri: URI? = null,
+        printerInfo: String? = null,
+        printerLocation: String? = null,
         ppdName: String? = null, // virtual PPD 'everywhere' is not supported by all CUPS versions
-        ppdInputStream: InputStream? = null
-    ) =
-        exchange(cupsPrinterRequest(
-            CupsAddModifyPrinter, printerName, deviceUri, ppdName, printerInfo, printerLocation
-        ).apply {
-            documentInputStream = ppdInputStream
-        })
+        ppdInputStream: InputStream? = null,
+        printerIsTemporary: Boolean? = null
+    ) = exchangeCupsPrinterRequest(
+        CupsAddModifyPrinter,
+        printerName,
+        deviceUri,
+        ppdName,
+        printerInfo,
+        printerLocation,
+        ppdInputStream,
+        printerIsTemporary,
+    )
 
     fun deletePrinter(printerName: String) =
-        exchange(ippRequest(CupsDeletePrinter, cupsPrinterUri(printerName)))
+        exchangeCupsPrinterRequest(CupsDeletePrinter, printerName)
 
     // https://www.cups.org/doc/spec-ipp.html#CUPS_CREATE_LOCAL_PRINTER
     fun createLocalPrinter(
@@ -97,24 +106,29 @@ open class CupsClient(
         printerLocation: String?,
         ppdName: String? // virtual PPD 'everywhere' is supported asynchronous
     ) =
-        exchange(
-            cupsPrinterRequest(
-                CupsCreateLocalPrinter, printerName, deviceUri, ppdName, printerInfo, printerLocation
-            )
+        exchangeCupsPrinterRequest(
+            CupsCreateLocalPrinter,
+            printerName,
+            deviceUri,
+            ppdName,
+            printerInfo,
+            printerLocation
         )
 
-    // -----------------------------------------------------------------
-    // build request for CupsCreateLocalPrinter and CupsAddModifyPrinter
-    // -----------------------------------------------------------------
+    // ---------------------------------------------------
+    // build and exchange requests for a named CupsPrinter
+    // ---------------------------------------------------
 
-    fun cupsPrinterRequest(
+    protected fun exchangeCupsPrinterRequest(
         operation: IppOperation,
         printerName: String,
         deviceUri: URI? = null,
         ppdName: String? = null,
         printerInfo: String? = null,
-        printerLocation: String? = null
-    ) =
+        printerLocation: String? = null,
+        ppdInputStream: InputStream? = null,
+        printerIsTemporary: Boolean? = null,
+    ) = exchange(
         ippRequest(operation, cupsPrinterUri(printerName)).apply {
             with(createAttributesGroup(Printer)) {
                 attribute("printer-name", NameWithoutLanguage, printerName.toIppString())
@@ -122,8 +136,11 @@ open class CupsClient(
                 ppdName?.let { attribute("ppd-name", NameWithoutLanguage, it.toIppString()) }
                 printerInfo?.let { attribute("printer-info", TextWithoutLanguage, it.toIppString()) }
                 printerLocation?.let { attribute("printer-location", TextWithoutLanguage, it.toIppString()) }
+                printerIsTemporary?.let { attribute("printer-is-temporary", IppTag.Boolean, printerIsTemporary) }
             }
+            ppdInputStream?.let { documentInputStream = ppdInputStream }
         }
+    )
 
     //----------------------
     // delegate to IppClient
@@ -135,15 +152,16 @@ open class CupsClient(
     fun exchange(ippRequest: IppRequest) =
         ippClient.exchange(ippRequest)
 
-    fun basicAuth(user: String, password: String) {
+    fun basicAuth(user: String, password: String) =
         ippClient.basicAuth(user, password)
-    }
 
     //-----------------------
     // delegate to IppPrinter
     //-----------------------
 
-    val ippPrinter = IppPrinter(cupsUri, ippClient = ippClient, getPrinterAttributesOnInit = false)
+    val ippPrinter: IppPrinter by lazy {
+        IppPrinter(cupsUri, ippClient = ippClient, getPrinterAttributesOnInit = false)
+    }
 
     fun createPrinterSubscription(
         // https://datatracker.ietf.org/doc/html/rfc3995#section-5.3.3.4.2
@@ -172,7 +190,7 @@ open class CupsClient(
         // validate ipp scheme
         if (!deviceUri.scheme.startsWith("ipp")) throw IllegalArgumentException(deviceUri.toString())
 
-        createLocalPrinter(printerName, deviceUri, printerInfo, printerLocation, ppdName = "everywhere").run {
+        createLocalPrinter(printerName, deviceUri, printerInfo, printerLocation, ppdName = "everywhere").apply {
             log.info { "$statusMessage ${printerGroup.getValues<List<URI>>("printer-uri-supported")}" }
         }
 
@@ -188,11 +206,7 @@ open class CupsClient(
             log.info { this }
 
             // make printer permanent
-            exchange(ippRequest(CupsAddModifyPrinter, cupsPrinterUri(printerName)).apply {
-                createAttributesGroup(Printer).apply {
-                    attribute("printer-is-temporary", IppTag.Boolean, false)
-                }
-            })
+            addModifyPrinter(printerName, printerIsTemporary = false)
 
             // make printer operational
             enable()
