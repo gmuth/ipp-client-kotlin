bdb0b3df4d2beeda263a8befe3e79539b2b8dbc8
Gerhard Muth <gerhard.muth@gmx.de>
2024-05-15 00:40:32 +0200

    do not create work directory anymore

--- src/main/kotlin/de/gmuth/ipp/client/CupsClient.kt
+++ src/main/kotlin/de/gmuth/ipp/client/CupsClient.kt
@@ -32,7 +32,7 @@ class CupsClient(
 
     private val cupsServer =
         IppPrinter(cupsUri, ippClient = ippClient, getPrinterAttributesOnInit = false)
-            .apply { workDirectory = cupsClientWorkDirectory.createDirectoryIfNotExists(false) }
+            .apply { workDirectory = cupsClientWorkDirectory }
 
     init {
         if (cupsUri.scheme == "ipps") config.trustAnyCertificateAndSSLHostname()
