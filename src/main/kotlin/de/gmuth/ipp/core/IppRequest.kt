5a8b9ba91cb589db083b0a9388a9c90a0ff5ecc6
Gerhard Muth <gerhard.muth@gmx.de>
2024-09-25 21:46:34 +0200

    use toSeconds()

--- src/main/kotlin/de/gmuth/ipp/core/IppRequest.kt
+++ src/main/kotlin/de/gmuth/ipp/core/IppRequest.kt
@@ -63,8 +63,8 @@ class IppRequest : IppMessage {
         attribute("notify-pull-method", Keyword, "ippget")
         notifyJobId?.let { attribute("notify-job-id", Integer, it) }
         notifyEvents?.let { attribute("notify-events", Keyword, it) }
-        notifyTimeInterval?.let { attribute("notify-time-interval", Integer, it.toMillis() / 1000) }
-        notifyLeaseDuration?.let { attribute("notify-lease-duration", Integer, it.toMillis() / 1000) }
+        notifyTimeInterval?.let { attribute("notify-time-interval", Integer, it.toSeconds()) }
+        notifyLeaseDuration?.let { attribute("notify-lease-duration", Integer, it.toSeconds()) }
     }
 
 }
