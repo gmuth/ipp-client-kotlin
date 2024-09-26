cfce81f0382adce63a32d2e25872753c60b0b287
Gerhard Muth <gerhard.muth@gmx.de>
2024-09-26 23:34:26 +0200

    updated version to 3.2

--- src/main/kotlin/de/gmuth/ipp/client/IppConfig.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppConfig.kt
@@ -23,7 +23,7 @@ class IppConfig(
 
     // HTTP config
     var timeout: Duration = Duration.ofSeconds(30),
-    var userAgent: String? = "ipp-client/3.1",
+    var userAgent: String? = "ipp-client/3.2",
     var password: String? = null,
     var sslContext: SSLContext? = null,
     // trust any certificate: sslContextForAnyCertificate()
