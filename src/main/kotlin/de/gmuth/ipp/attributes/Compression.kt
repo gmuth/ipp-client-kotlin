59a73fac2a4edaf610a116820e68e2af0f163ff0
Gerhard Muth <gerhard.muth@gmx.de>
2024-10-02 19:38:34 +0200

    attribute 'compression'

---
+++ src/main/kotlin/de/gmuth/ipp/attributes/Compression.kt
@@ -0,0 +1,21 @@
+package de.gmuth.ipp.attributes
+
+/**
+ * Copyright (c) 2024 Gerhard Muth
+ */
+
+import de.gmuth.ipp.core.IppAttribute
+import de.gmuth.ipp.core.IppAttributeBuilder
+import de.gmuth.ipp.core.IppAttributesGroup
+import de.gmuth.ipp.core.IppTag.Keyword
+
+enum class Compression(private val keyword: String) : IppAttributeBuilder {
+
+    COMPRESS("compress"), // RFC 1977
+    DEFLATE("deflate"), // RFC 1951
+    GZIP("gzip"), // RFC 1952
+    NONE("none");
+
+    override fun buildIppAttribute(printerAttributes: IppAttributesGroup) =
+        IppAttribute("compression", Keyword, keyword)
+}
