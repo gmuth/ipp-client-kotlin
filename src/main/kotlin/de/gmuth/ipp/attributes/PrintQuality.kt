7bbe1fe1d17671e33e6eeae22f75d8a482a673a4
Gerhard Muth <gerhard.muth@gmx.de>
2024-11-11 21:09:27 +0100

    code visibility

--- src/main/kotlin/de/gmuth/ipp/attributes/PrintQuality.kt
+++ src/main/kotlin/de/gmuth/ipp/attributes/PrintQuality.kt
@@ -1,7 +1,7 @@
 package de.gmuth.ipp.attributes
 
 /**
- * Copyright (c) 2020-2023 Gerhard Muth
+ * Copyright (c) 2020-2024 Gerhard Muth
  */
 
 import de.gmuth.ipp.core.IppAttribute
@@ -9,7 +9,7 @@ import de.gmuth.ipp.core.IppAttributeBuilder
 import de.gmuth.ipp.core.IppAttributesGroup
 import de.gmuth.ipp.core.IppTag.Enum
 
-enum class PrintQuality(private val code: Int) : IppAttributeBuilder {
+enum class PrintQuality(val code: Int) : IppAttributeBuilder {
 
     Draft(3), Normal(4), High(5);
 
