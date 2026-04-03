28aa1686f2be603a43003291d4528de5538e9df7
Gerhard Muth <gerhard.muth@gmx.de>
2026-04-03 13:28:35 +0200

    3.6-SNAPSHOT, Kotlin 1.9, sonarqube 5.1

--- src/main/kotlin/de/gmuth/ipp/client/IppConfig.kt
+++ src/main/kotlin/de/gmuth/ipp/client/IppConfig.kt
@@ -1,7 +1,7 @@
 package de.gmuth.ipp.client
 
 /**
- * Copyright (c) 2021-2025 Gerhard Muth
+ * Copyright (c) 2021-2026 Gerhard Muth
  */
 
 import java.nio.charset.Charset
@@ -23,7 +23,7 @@ class IppConfig(
 
     // HTTP config
     var timeout: Duration = Duration.ofSeconds(30),
-    var userAgent: String? = "ipp-client/3.5",
+    var userAgent: String? = "ipp-client/3.6",
     var password: String? = null,
     var sslContext: SSLContext? = null,
     // trust any certificate: sslContextForAnyCertificate()
