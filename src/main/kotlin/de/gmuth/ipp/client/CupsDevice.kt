268d1a471ec9ee611f25f15da2152150a27b2a90
Gerhard Muth <gerhard.muth@gmx.de>
2026-04-04 22:50:00 +0200

    implemented CupsGetDevices operation

---
+++ src/main/kotlin/de/gmuth/ipp/client/CupsDevice.kt
@@ -0,0 +1,38 @@
+package de.gmuth.ipp.client
+
+/**
+ * Copyright (c) 2026 Gerhard Muth
+ */
+
+import de.gmuth.ipp.core.IppAttributesGroup
+import de.gmuth.ipp.core.IppString
+import java.net.URI
+
+class CupsDevice(
+    val id: String,
+    val uri: URI,
+    val info: String,
+    val _class: String,
+    val location: String,
+    val makeAndModel: String
+) {
+    constructor(attributes: IppAttributesGroup) : this(
+        attributes.getValue<IppString>("device-id").text,
+        attributes.getValueAsURI("device-uri"),
+        attributes.getValue<IppString>("device-info").text,
+        attributes.getKeywordOrName("device-class"),
+        attributes.getValue<IppString>("device-location").text,
+        attributes.getValue<IppString>("device-make-and-model").text
+    )
+
+    override fun toString() = StringBuilder().run {
+        append(info)
+        append(", $makeAndModel")
+        append(", class=\"$_class\"")
+        append(", uri=\"$uri\"")
+        if (location.isNotEmpty()) append(", location=$location")
+        if (id.isNotEmpty()) append(", id=\"$id\"")
+        toString()
+    }
+
+}
